3715473d5ee6e7d6b7901a50a722c3710b8830c4
Helder Correia <me@heldercorreia.com>
2011-10-26 15:10:17 +0000

    Added Administrator account creation

---
+++ src/model/Administrator.java
@@ -0,0 +1,22 @@
+
+package model;
+
+public class Administrator {
+
+    private String username;
+    private String password;
+
+    public Administrator(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+}
---
+++ test/model/AdministratorTest.java
@@ -0,0 +1,21 @@
+
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AdministratorTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void canCreateAccount() {
+        Administrator admin = new Administrator("dummy", "password");
+        assertEquals("dummy", admin.getUsername());
+        assertEquals("password", admin.getPassword());
+    }
+}
