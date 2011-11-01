4c34ccea1aef2b9090711a6322ad6ad81969768b
Helder Correia <me@heldercorreia.com>
2011-11-01 14:18:02 -0100

    Added the test suite
    Please add your test classes to this file

---
+++ test/pt/uac/cafeteria/TestSuite.java
@@ -0,0 +1,13 @@
+
+package pt.uac.cafeteria;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import pt.uac.cafeteria.model.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+    AcceptanceTests.class,
+    AdministratorTest.class,
+})
+public class TestSuite {}
