d4f84ca42a4e920d0ce24bd56d92f51c100e92a6
Helder Correia <me@heldercorreia.com>
2011-12-22 19:28:55 -0100

    Changed DataMapper scope to public
    - Changed scope to public so it can be used in other packages as a generic for any data mapper;
    - Switched K (key) for I (id) in generics;
    - Swapped order of methods to suggest CRUD (create, read, update, delete).

--- src/pt/uac/cafeteria/model/persistence/DataMapper.java
+++ src/pt/uac/cafeteria/model/persistence/DataMapper.java
@@ -1,8 +1,9 @@
 
 package pt.uac.cafeteria.model.persistence;
 
-interface DataMapper<T, K> {
-    public K insert(T o);
+public interface DataMapper<T, I> {
+    public I insert(T o);
+    public T find(I id);
+    public int update(T o);
     public boolean delete(T o);
-    public T find(K key);
 }
