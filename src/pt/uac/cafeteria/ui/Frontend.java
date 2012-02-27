f134035fa711c5d14bc402eed5ebc4369936aa1e
Helder Correia <me@heldercorreia.com>
2012-02-27 20:43:19 -0100

    Added licensing information.

--- src/pt/uac/cafeteria/ui/Frontend.java
+++ src/pt/uac/cafeteria/ui/Frontend.java
@@ -1,4 +1,24 @@
 
+/**
+ * Cafeteria management application
+ * Copyright (c) 2011, 2012 Paulo Silva
+ * 
+ * This file is part of Cafeteria.
+ * 
+ * Cafeteria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Cafeteria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cafeteria.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pt.uac.cafeteria.ui;
 
 import java.awt.Dimension;
